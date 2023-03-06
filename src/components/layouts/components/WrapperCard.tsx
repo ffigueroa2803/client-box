c39e92aed2a823d4d52a0faa7d250e7d639a430e
Jean Flores Alvarez <ajiliof@gmail.com>
2023-03-06 10:24:10 -0500

    advanceding page/income

--- src/components/layouts/components/WrapperCard.tsx
+++ src/components/layouts/components/WrapperCard.tsx
@@ -2,7 +2,13 @@ import { ReactNode } from 'react'
 
 interface IProps {
   children: ReactNode
+  tittle?: string
 }
-export const WrapperCard = ({ children }: IProps) => (
-  <div className="p-6 text-custom1 dark:text-custom5  bg-white dark:bg-darkCustom1 rounded-2xl">{children}</div>
+export const WrapperCard = ({ children, tittle }: IProps) => (
+  <div className="p-6 w-full text-custom1 dark:text-custom5  bg-white dark:bg-darkCustom1 rounded-2xl relative">
+    <div className="absolute top-2 left-6 font-bold lg:text-lg">
+      {tittle && tittle}
+    </div>
+    <div className={`w-full ${tittle && 'mt-6'}`}>{children}</div>
+  </div>
 )
--- src/pages/income.tsx
+++ src/pages/income.tsx
@@ -8,34 +8,64 @@ const Income = () => {
         TituloSeccion="Ingresos"
         // history={[{ description: 'Proyectos' }]}
       />
-      <WrapperCard>
-        <div className="flex flex-row gap-y-2 flex-wrap justify-between items-end">
-          {/* <div className="w-1/2"> */}
-          <Input
-            classNames="flex flex-col gap-2 w-full md:w-1/2  "
-            label="Buscar Numero Recibo"
-            placeholder="Numero recibo"
-            value={''}
-            maxLength={7}
-            name="cui"
-            handleChange={() => console.log('')}
-          />
-          {/* </div> */}
-          {/* <div className="flex justify-center w-1/2"> */}
-          <div className="w-full flex justify-end md:w-1/2 ">
-            <button
-              className="btn btn-primary w-60 flex gap-1 "
-              //   onClick={BottomEvent.OnClick}
-            >
-              {/* <BottomEvent.Icon /> */}
+      <div className="flex flex-col lg:flex-row gap-4">
+        <WrapperCard tittle="Datos del Cliente">
+          <div className="flex flex-row gap-y-2 flex-wrap justify-between items-end">
+            {/* <div className="w-1/2"> */}
+            <Input
+              classNames="flex flex-col gap-2 w-full md:w-1/2  "
+              label="Buscar Numero Recibo"
+              placeholder="Numero recibo"
+              value={''}
+              maxLength={7}
+              name="cui"
+              handleChange={() => console.log('')}
+            />
+            {/* </div> */}
+            {/* <div className="flex justify-center w-1/2"> */}
+            <div className="w-full flex justify-end md:w-1/2 ">
+              <button
+                className="btn btn-primary w-60 flex gap-1 "
+                //   onClick={BottomEvent.OnClick}
+              >
+                {/* <BottomEvent.Icon /> */}
 
-              <p>Buscar</p>
-            </button>
+                <p>Buscar</p>
+              </button>
+            </div>
+
+            {/* </div> */}
           </div>
+        </WrapperCard>
+        <WrapperCard tittle="Datos del Producto">
+          <div className="flex flex-row gap-y-2 flex-wrap justify-between items-end">
+            {/* <div className="w-1/2"> */}
+            <Input
+              classNames="flex flex-col gap-2 w-full md:w-1/2  "
+              label="Buscar Numero Recibo"
+              placeholder="Numero recibo"
+              value={''}
+              maxLength={7}
+              name="cui"
+              handleChange={() => console.log('')}
+            />
+            {/* </div> */}
+            {/* <div className="flex justify-center w-1/2"> */}
+            <div className="w-full flex justify-end md:w-1/2 ">
+              <button
+                className="btn btn-primary w-60 flex gap-1 "
+                //   onClick={BottomEvent.OnClick}
+              >
+                {/* <BottomEvent.Icon /> */}
 
-          {/* </div> */}
-        </div>
-      </WrapperCard>
+                <p>Buscar</p>
+              </button>
+            </div>
+
+            {/* </div> */}
+          </div>
+        </WrapperCard>
+      </div>
     </div>
   )
 }
